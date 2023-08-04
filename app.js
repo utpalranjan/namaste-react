014c018f3637b562cd3f70af5d603c535588a03f
Utpal Ranjan <utpal.ranjan.decimal.co.in>
2023-08-04 19:44:45 +0530

    Episode 3 namaste React

--- app.js
+++ app.js
@@ -1,40 +1,37 @@
-/* <div id = "parent">
-    <div id = "child">
-        <h1>I am an H1 tag</h1>
-        <h2>I am an H2 tag</h2>
-    </div>
-    <div id = "child2">
-        <h1>I am an H1 tag</h1>
-        <h2>I am an H2 tag</h2>
-    </div>
-</div>  */
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-////How can we achive this 
+const heading = React.createElement("h1", { id: "heading" }, "Namsate React");
 
+//JSX ==>React.createElement ==> ReactElement - JS Object ==>HTMLElement(render)
 
+//React Element
+const jsxHeading = (
+  <h1 id="heading" tabIndex="5">
+    Namsate React using JSX1
+  </h1>
+);
 
+//React Functional Component
+const Title = () => (
+  <h1 id="heading" tabIndex="5">
+    Namsate React using JSX
+  </h1>
+);
+//Calling one component inside another component is called component composition
+// here React element can be use in component by writing inside {}
+const HeadingComponent = () => (
+  <div id="parent">
+    {/* {jsxHeading} */}
+    <Title />
+    {Title()}
+    <Title></Title>
+    <h1 className="heading">Namaste React from functional Component</h1>
+  </div>
+);
 
+//console.log(jsxHeading);
 
-
-// const heading =
-//  React.createElement("h1",{
-//  id:"heading"
-// }, "Hello world form React"); 
-
-const parent = 
-React.createElement("div",{id:"parent"},
-[React.createElement("div",{id:"child"},[
-    React.createElement("h1",{},"I am an H1 tag"),
-    React.createElement("h2",{},"I am an H2 tag"),
-]),
-React.createElement("div",{id:"child2"},[
-    React.createElement("h1",{},"I am an H1 tag"),
-    React.createElement("h2",{},"I am an H2 tag"),
-])]
-)
-
-console.log(parent) // object
-//creating tag from react
-const root = ReactDOM.createRoot(document.getElementById("root")); // creating a root for adding value
-
-root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeadingComponent />); //for calling functional components
+//root.render(jsxHeading); //for calling react element
